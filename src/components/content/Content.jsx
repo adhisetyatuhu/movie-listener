368c640da164ae872616c2cf28ecfdac8f622277
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-11-28 11:18:18 +0700

    Feat: we can now add banner on card as child component

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -19,11 +19,11 @@ const Banner = (props) => {
 }
 
 const Card = (props) => {
-    const classes = props.classes;
-    const height = props.height;
-    const width = props.width;
+    const classes = props.classes ? props.classes : "";
+    const height = props.height ? props.height : "10rem";
+    const width = props.width ? props.width : "8rem";
+    const borderRadius = props.borderRadius ? props.borderRadius : "0.5em";
     const containerHeight = props.containerHeight;
-    const borderRadius = props.borderRadius;
 
     const cardStyle = {
         position: "relative",
@@ -59,7 +59,7 @@ const Card = (props) => {
         <>
             <div className={classes} style={containerStyle}>
                 <div style={cardStyle}>
-                    <Banner text="New" borderRadius={borderRadius} />
+                    {props.children}
                     <figure style={figureStyle}></figure>
                     <figcaption style={figcaptionStyle}>Title yang Panjang Apakah yang Terjadi?</figcaption>
                 </div>
@@ -74,12 +74,17 @@ const Content = () => {
     const height = `${size}rem`;
     const width = `${size * 0.8}rem`;
     const containerHeight = `${size + 2}rem`;
+    const borderRadius = "0.5rem";
 
     return (
         <>
             <div className="d-flex gap-2">
-                <Card height={height} width={width} containerHeight={containerHeight} borderRadius="0.5em" />
-                <Card height={height} width={width} containerHeight={containerHeight} borderRadius="0.5em" />
+                <Card height={height} width={width} containerHeight={containerHeight} borderRadius={borderRadius}></Card>
+                <Card height={height} width={width} containerHeight={containerHeight} borderRadius={borderRadius}>
+                    <Banner text="New" borderRadius={borderRadius} />
+                </Card>
+                <Card height={height} width={width} containerHeight={containerHeight} borderRadius={borderRadius}></Card>
+                <Card height={height} width={width} containerHeight={containerHeight} borderRadius={borderRadius}></Card>
             </div>
         </>
     );
