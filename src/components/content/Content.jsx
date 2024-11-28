b772f0a53e86e799ed13d5bc03a8c45bf64a1499
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-11-28 10:43:45 +0700

    Feat: add size attributes on card component

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,31 +1,40 @@
 const Banner = (props) => {
+    const borderRadius = props.borderRadius;
+    const text = props.text;
+
     const bannerStyle = {
         position: "absolute",
         backgroundColor: "red",
         color: "white",
-        borderRadius: "0.5rem 0 0 0",
+        borderRadius: `${borderRadius} 0 0 0`,
         fontSize: "0.75em",
         padding: "0 0.5em",
         top: "0"
     }
     return (
         <>
-            <span style={bannerStyle}>{props.text}</span>
+            <span style={bannerStyle}>{text}</span>
         </>
     );
 }
 
 const Card = (props) => {
+    const classes = props.classes;
+    const height = props.height;
+    const width = props.width;
+    const containerHeight = props.containerHeight;
+    const borderRadius = props.borderRadius;
+
     const cardStyle = {
         position: "relative",
-        height: "10rem",
-        width: "8rem",
+        height: height,
+        width: width,
         overflow: "hidden"
     }
     
     const figureStyle = {
-        height: "10rem",
-        borderRadius: "0.5rem",
+        height: height,
+        borderRadius: borderRadius,
         backgroundColor: "#777"
     };
 
@@ -37,16 +46,20 @@ const Card = (props) => {
         left: "50%",
         transform: "translateX(-50%)",
         textAlign: "center",
-        width: "8rem",
+        width: width,
         backgroundColor: "rgba(0, 0, 0, 0.5)",
-        borderRadius: "0 0 0.5rem 0.5rem"
+        borderRadius: `0 0 ${borderRadius} ${borderRadius}`
+    }
+
+    const containerStyle = {
+        height: containerHeight,
     }
 
     return (
         <>
-            <div className={props.classes} style={{ height: "12rem"}}>
+            <div className={classes} style={containerStyle}>
                 <div style={cardStyle}>
-                    <Banner text="New" />
+                    <Banner text="New" borderRadius={borderRadius} />
                     <figure style={figureStyle}></figure>
                     <figcaption style={figcaptionStyle}>Title yang Panjang Apakah yang Terjadi?</figcaption>
                 </div>
@@ -56,12 +69,17 @@ const Card = (props) => {
 }
 
 const Content = () => {
+    const size = 15;
+    // height and width ratio = 10:8
+    const height = `${size}rem`;
+    const width = `${size * 0.8}rem`;
+    const containerHeight = `${size + 2}rem`;
+
     return (
         <>
             <div className="d-flex gap-2">
-                <Card />
-                <Card />
-                <Card />
+                <Card height={height} width={width} containerHeight={containerHeight} borderRadius="0.5em" />
+                <Card height={height} width={width} containerHeight={containerHeight} borderRadius="0.5em" />
             </div>
         </>
     );
