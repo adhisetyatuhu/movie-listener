b899871d3ff70cddeeafb066f8707f1456df608f
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-11-28 11:37:18 +0700

    Chore: refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header/Header'
-import Content from './components/content/Content'
+import Header from './components/Header'
+import Content from './components/Content'
 
 function App() {
 
