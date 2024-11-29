ee16f431e658d68188655383a6559d827dc292b2
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-11-29 13:13:11 +0700

    Feat: add recommendation on search result

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -4,7 +4,17 @@ const SearchResult = ({keyword, }) => {
     return (
         <>
             <h2 className="text-light mt-5">Search "{keyword}"</h2>
-            <div className="d-flex gap-2">
+            <div className="d-flex flex-wrap gap-2">
+                <Card text="Title yang muncul dari hasil pencarian"></Card>
+                <Card text="Title yang muncul dari hasil pencarian"></Card>
+                <Card text="Title yang muncul dari hasil pencarian"></Card>
+                <Card text="Title yang muncul dari hasil pencarian"></Card>
+            </div>
+
+            <h2 className="text-light mt-5">You might be interested</h2>
+            <div className="d-flex flex-wrap gap-2">
+                <Card text="Title yang muncul dari hasil pencarian"></Card>
+                <Card text="Title yang muncul dari hasil pencarian"></Card>
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
