e49ee60d5e342750876fdd9285fbe59f71f78df9
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-02 13:50:19 +0700

    Fix: search page styling with tailwindcss

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -3,16 +3,16 @@ import Card from "../components/Card";
 const SearchResult = ({keyword, }) => {
     return (
         <>
-            <h2 className="text-light mt-5">Search "{keyword}"</h2>
-            <div className="d-flex flex-wrap gap-2">
+            <h2 className="text-white text-3xl mt-12">Search "{keyword}"</h2>
+            <div className="flex flex-wrap gap-4 py-4">
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
             </div>
 
-            <h2 className="text-light mt-5">You might be interested</h2>
-            <div className="d-flex flex-wrap gap-2">
+            <h2 className="text-white text-3xl mt-12">You might be interested</h2>
+            <div className="flex flex-wrap gap-4 py-4">
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
                 <Card text="Title yang muncul dari hasil pencarian"></Card>
