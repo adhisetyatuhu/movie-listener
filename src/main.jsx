9964be177fccb0d6a5cbd97b2df129e3cf904bb4
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-02 16:31:21 +0700

    Fix: remove bootstrap from index

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
