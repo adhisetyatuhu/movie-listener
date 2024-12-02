a6f59cf819b7e1638cfc757b3d1b8808e1671846
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-02 14:02:13 +0700

    Feat: add error page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,10 @@
+const Error = () => {
+    return (
+        <>
+            <div className="text-center text-6xl font-bolder mt-24 lg:mt-64">ERROR 404</div>
+            <div className="text-center text-2xl mt-4">We can't find the page you're looking for</div>
+        </>
+    );
+}
+
+export default Error;
