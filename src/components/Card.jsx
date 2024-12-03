6867a8d09ce21efc3be9dec47adff2a128285748
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-03 18:06:35 +0700

    Feat: add data fetching for home page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,20 @@ const Card = (props) => {
     const height = props.height ? props.height : "15rem";
     const width = props.width ? props.width : "12rem";
     const containerHeight = props.containerHeight ? props.containerHeight : "17rem";
+    const title = props.data?.title;
+    const releasedYear = props.data?.release_date.split('-')[0];
+    const posterUrl = `https://image.tmdb.org/t/p/w500${props.data?.poster_path}`
 
     return (
         <>
             <div className={classes} style={{"height": containerHeight}}>
                 <div className="relative overflow-hidden rounded-lg" style={{"height": height, "width": width}}>
                     {props.children}
-                    <figure className="bg-[#777]" style={{"height": height}}></figure>
-                    <figcaption className="absolute rounded-b-lg bottom-0 left-1/2 -translate-x-1/2 text-white text-xs text-center bg-black/50" style={{"width": width}}>
-                        {props.text}
+                    <figure className="bg-[#777]" style={{"height": height}}>
+                        <img src={posterUrl} />
+                    </figure>
+                    <figcaption className="absolute py-2 rounded-b-lg bottom-0 left-1/2 -translate-x-1/2 text-white text-xs text-center bg-black/70" style={{"width": width}}>
+                        {title}<span>{releasedYear ? " ("+releasedYear+")": ""}</span>
                     </figcaption>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
+import { useEffect, useState } from "react";
 import Card from "../components/Card";
 import CardBanner from "../components/CardBanner";
+import tmdb from "../utils/axios";
 
-const Billboard = () => {
+const Billboard = (props) => {
+    const imgUrl = `https://image.tmdb.org/t/p/original${props.data?.backdrop_path}`;
     return (
         <>
             <div className="relative">
-                <figure className="h-[28rem] bg-[#777]"></figure>
-                <div className="absolute p-8 text-white max-w-2xl bottom-0 bg-gradient-to-r from-black/40 to-black/0">
-                    <h2 className="text-3xl">Some Title</h2>
-                    <p className="my-2">A description. It can be very very long description, anything will do. This is also a description. We will test how long it can hold.</p>
-                    <button className="py-2 px-4 my-2 border bg-black/10 rounded-lg hover:bg-white/30 active:bg-black/10">More Info</button>
+                <figure className="h-[28rem] overflow-hidden">
+                    <img className="object-cover h-[28rem] lg:h-fit lg:w-full" src={ imgUrl } />
+                </figure>
+                <div className="absolute p-8 text-white max-w-4xl bottom-0 bg-gradient-to-r from-black/60 to-black/0">
+                    <h2 className="text-3xl drop-shadow shadow-black" style={{textShadow : "0 0 1px #000000"}}>{ props.data?.title }</h2>
+                    <p className="my-2 hidden sm:block" style={{textShadow : "0 0 2px #000000"}}>{ props.data?.overview }</p>
+                    <button className="py-1 md:py-2 px-4 my-8 md:my-2 border bg-black/10 rounded-lg hover:bg-white/30 active:bg-black/10">More Info</button>
                 </div>
             </div>
         </>
@@ -17,6 +22,25 @@ const Billboard = () => {
 }
 
 const Home = () => {
+    const [billboardData, setBillboardData] = useState(null);
+    const [movies, setMovies] = useState([]);
+    
+    const fetchMovies = async () => {
+        try {
+            const { data } = await tmdb.get('/discover/movie');
+            const randomNum = Math.floor(Math.random() * 20);
+            setMovies(data?.results);
+            setBillboardData(movies[randomNum]);
+            // console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
     const size = 15;
     // height and width ratio = 10:8
     const height = `${size}rem`;
@@ -26,19 +50,13 @@ const Home = () => {
 
     return (
         <>
-            <div className="py-3"><Billboard /></div>
+            <div className="py-3"><Billboard data={billboardData} /></div>
             <div className="flex gap-2 overflow-scroll hide-scrollbar">
-                <Card text="Title yang Panjang Apakah yang Terjadi?" height={height} width={width} containerHeight={containerHeight}>
-                    <CardBanner text="New" />
-                </Card>
-                <Card text="Title yang Panjang Apakah yang Terjadi?" height={height} width={width} containerHeight={containerHeight}>
-                    <CardBanner text="New" />
-                </Card>
-                <Card text="Title yang Panjang Apakah yang Terjadi?" height={height} width={width} containerHeight={containerHeight}></Card>
-                <Card text="Title yang Panjang Apakah yang Terjadi?" height={height} width={width} containerHeight={containerHeight}></Card>
-                <Card text="Title yang Panjang Apakah yang Terjadi?" height={height} width={width} containerHeight={containerHeight}></Card>
-                <Card text="Title yang Panjang Apakah yang Terjadi?" height={height} width={width} containerHeight={containerHeight}></Card>
-                <Card text="Title yang Panjang Apakah yang Terjadi?" height={height} width={width} containerHeight={containerHeight}></Card>
+                {
+                    movies.map(movie => {
+                        return <Card key={movie.id} data={movie} height={height} width={width} />
+                    })
+                }
             </div>
         </>
     );
